f20bb9bfa4c78b77614c5e4f1f1e064eaa1c8a72
Renz Rackhold <rackhold@gmail.com>
2026-04-27 02:31:46 +0800

    fix: update layout styles for improved Nativewind integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import "@/global.css";
+
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
   return <Stack />;
--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
     </View>
