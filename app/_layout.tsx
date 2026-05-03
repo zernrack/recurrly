a64f3ab975d280d0372247ff6df8908c3d020b77
Renz Rackhold <rackhold@gmail.com>
2026-05-04 01:29:16 +0800

    feat: add custom fonts and update layout to handle font loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,26 @@
 import "@/global.css";
 
-import { Stack } from "expo-router";
-import React from "react";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync(); // Hide the splash screen once fonts are loaded
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null; // Don't render anything until fonts are loaded
+
   return <Stack screenOptions={{ headerShown: false }} />; // Hide the header for all screens in the stack
 }
